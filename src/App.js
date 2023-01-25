7e1105849aff92f3a884f3cb5ca90bb045b529cc
Tihifas <tdbratberg@gmail.com>
2023-01-25 18:50:17 +0100

    Added Image to LinkBox component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 import { Header } from './Components/Header';
 import { LinkBox } from './Components/LinkBox';
 import { PortfolioItem } from './Components/PortfolioItem';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import koalaImg from './Images/DALL·E 2023-01-25 18.07.04 - Koala Programmer by Leonardo Da Vinci.png';
+import guineaPigImg from './Images/DALL·E 2023-01-25 18.07.07 - Guinea pig having an existential crisis in a grocery store.png';
+import uglyDucklingImg from './Images/DALL·E 2023-01-25 18.07.12 - An old school scifi book cover about the ugly duckling.png';
+import slenderCruellaImg from './Images/DALL·E 2023-01-25 18.07.19 - The child of slenderman and Cruella de Vil, digital art.png';
+import winnieSupperImg from './Images/DALL·E 2023-01-25 18.07.24 - The last supper painting by Leonardo with characters from winnie the pooh.png';
+
 
 function App() {
   return (
@@ -15,14 +21,11 @@ function App() {
               <div className='app-container'>
                 <Header />
                 <div className='linkbox-container'>
-                  <LinkBox />
-                  <LinkBox />
-                  <LinkBox />
-                  <LinkBox />
-                  <LinkBox />
-                  <LinkBox />
-                  <LinkBox />
-                  <LinkBox />
+                  <LinkBox heading={"Koala C. Martin"} img={koalaImg}/>
+                  <LinkBox heading={"The Supermarked"} img={guineaPigImg}/>
+                  <LinkBox heading={"Look Away"} img={uglyDucklingImg}/>
+                  <LinkBox heading={"A Crime Against Nature"} img={slenderCruellaImg}/>
+                  <LinkBox heading={"Who framed Winnie?"} img={winnieSupperImg}/>
                 </div>
               </div>
 
--- src/Components/LinkBox.js
+++ src/Components/LinkBox.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-
-export const LinkBox = () => {
+export const LinkBox = ({heading, img}) => {
     return (
         <Link to='/link1' className='linkbox-outer'>
             <div className='linkbox-content'>
-                <div className='linkbox-heading'> link1 </div>
+                <div className='linkbox-heading'> {heading} </div>
+                <img src={img} className='linkbox-img'/>
             </div>
         </Link>
     )
