f967ae1fa6efa79969deda04a95e1208f3951b19
Tihifas <tdbratberg@gmail.com>
2023-02-11 15:58:19 +0100

    Before triangle arrow point refactor

--- src/Views/GeometryExperiments.js
+++ src/Views/GeometryExperiments.js
@@ -18,17 +18,23 @@ import { useState, useEffect } from 'react';
 //   }  
 
 export const GeometryExperiments = () => {
-    let sideLength = '100px';
+  let sideL = 100;
+    let triangleDist = (0.87+1)*sideL;
+    let hexagonSideL = triangleDist;
     // const {windowHeight, windowWidth} = useWindowDimensions()
-    let hexagonSideL = window.innerHeight * 0.1 + 'px';
   return (
-    <div className='layout-container blue-theme' style={{marginLeft: '30%', marginTop: '30%'}}>
+    <div className='layout-container blue-theme' style={{marginLeft: '10%', marginTop: '10%'}}>
         {/* <div className='triangle layout-border-element'></div> */}
-        <Triangle sideLength={hexagonSideL} rotation={'-45deg'} left={'0px'} top={'0px'}/>
-        <Triangle sideLength={hexagonSideL} rotation={'45deg'} left={'100px'} top={'0px'}/>
-        <Triangle sideLength={hexagonSideL} rotation={'135deg'} left={'100px'} top={'100px'}/>
-        <Triangle sideLength={hexagonSideL} rotation={'225deg'} left={'0px'} top={'100px'}/>
-
+        {/* <Triangle sideLength={hexagonSideL} rotation={'-45deg'} left={0} top={0}/>
+        <Triangle sideLength={hexagonSideL} rotation={'45deg'} left={triangleDist} top={0}/>
+        <Triangle sideLength={hexagonSideL} rotation={'225deg'} left={0} top={triangleDist}/>
+        <Triangle sideLength={hexagonSideL} rotation={'135deg'} left={triangleDist} top={triangleDist}/> */}
+        {/* <Triangle sideLength={hexagonSideL} rotation={'0deg'} left={triangleDist} top={triangleDist}/> */}
+        {/* todo */}
+        <Triangle sideLength={hexagonSideL} rotation={'0deg'} left={0} top={0} color='red'/>
+        {/* <Triangle sideLength={hexagonSideL} rotation={'45deg'} left={0} top={0} color='blue'/>
+        <Triangle sideLength={hexagonSideL} rotation={'225deg'} left={0} top={triangleDist} color='green'/>
+        <Triangle sideLength={hexagonSideL} rotation={'135deg'} left={0} top={triangleDist} color='yellow'/> */}
     </div>
   )
 }
--- src/Views/Triangle.js
+++ src/Views/Triangle.js
@@ -1,13 +1,20 @@
 import React from 'react'
 
-export const Triangle = ({sideLength, rotation, left, top}) => {
+export const Triangle = ({ sideLength, rotation, left, top, color }) => {
+  let borderLengthArg = sideLength / 2 + 'px';
+  let leftPx = left + 'px';
+  let topPx = top + 'px';
   return (
     // var transformValue = 'rotate(' + rotation + ')';
     <div className='triangle' style={{
-        borderTop: sideLength + ' solid #f00',
-        borderLeft: sideLength + ' solid transparent',
-        borderRight: sideLength + ' solid transparent',
-        transform: 'rotate(' + rotation + ')', left: left, top: top}}> </div>
+      position: 'absolute',
+      width: 0,
+      height: 0,
+      borderTop: borderLengthArg + ' solid ' + color,
+      borderLeft: borderLengthArg + ' solid transparent',
+      borderRight: borderLengthArg + ' solid transparent',
+      transform: 'rotate(' + rotation + ')', left: leftPx, top: topPx
+    }}> </div>
     /* border-left: 20px solid transparent;
         border-right: 20px solid transparent;
         border-top: 20px solid #f00;*/
