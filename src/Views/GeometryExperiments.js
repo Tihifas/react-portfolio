68647b6fbd01e1a83560b5bca1e4dc129864f1ff
Tihifas <tdbratberg@gmail.com>
2023-02-11 18:15:35 +0100

    corner triangles. realized not be ligebenede

--- src/Views/GeometryExperiments.js
+++ src/Views/GeometryExperiments.js
@@ -4,37 +4,47 @@ import { useState, useEffect } from 'react';
 
 // export default function useWindowDimensions() {
 //     const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-  
+
 //     useEffect(() => {
 //       function handleResize() {
 //         setWindowDimensions(getWindowDimensions());
 //       }
-  
+
 //       window.addEventListener('resize', handleResize);
 //       return () => window.removeEventListener('resize', handleResize);
 //     }, []);
-  
+
 //     return windowDimensions;
 //   }  
 
 export const GeometryExperiments = () => {
-  let sideL = 100;
-    let triangleDist = (0.87+1)*sideL;
-    let hexagonSideL = triangleDist;
-    // const {windowHeight, windowWidth} = useWindowDimensions()
+  //left: 1,0625
+  //right: 0.355
+  
+//ALL THIS WAS WRONG. IT IS NOT A LIGEBENET TRIANGLE!!!
+
+  let sideL = 500 + .0;
+  let katete = sideL * 0.707;
+
+  let containerWidth = 2*katete + sideL;
+  let containerHeight = 2*katete;
+  // const {windowHeight, windowWidth} = useWindowDimensions()
   return (
-    <div className='layout-container blue-theme' style={{marginLeft: '10%', marginTop: '10%'}}>
-        {/* <div className='triangle layout-border-element'></div> */}
-        {/* <Triangle sideLength={hexagonSideL} rotation={'-45deg'} left={0} top={0}/>
-        <Triangle sideLength={hexagonSideL} rotation={'45deg'} left={triangleDist} top={0}/>
-        <Triangle sideLength={hexagonSideL} rotation={'225deg'} left={0} top={triangleDist}/>
-        <Triangle sideLength={hexagonSideL} rotation={'135deg'} left={triangleDist} top={triangleDist}/> */}
-        {/* <Triangle sideLength={hexagonSideL} rotation={'0deg'} left={triangleDist} top={triangleDist}/> */}
-        {/* todo */}
-        <Triangle sideLength={hexagonSideL} rotation={'0deg'} left={0} top={0} color='red'/>
-        {/* <Triangle sideLength={hexagonSideL} rotation={'45deg'} left={0} top={0} color='blue'/>
-        <Triangle sideLength={hexagonSideL} rotation={'225deg'} left={0} top={triangleDist} color='green'/>
-        <Triangle sideLength={hexagonSideL} rotation={'135deg'} left={0} top={triangleDist} color='yellow'/> */}
+    <div className='layout-container blue-theme' style={{ marginLeft: '2%', marginTop: '40%',
+                                                          backgroundColor: 'purple', marginBottom: '30%',
+                                                          width: containerWidth + 'px', height: containerHeight + 'px' }}>
+      {/* <Triangle sideLength={sideL} rotation={135 left={0} top={0}  color='red'/> */}
+      {/* <Triangle sideLength={sideL} rotation={225 left={triangleDist} top={0}  color='blue'/> */}
+      {/* <Triangle sideLength={sideL} rotation={45 left={0} top={triangleDist}  color='green'/> */}
+      {/* <Triangle sideLength={sideL} rotation={-45 left={triangleDist} top={triangleDist} color='yellow'/> */}
+      {/* todo: undo */}
+
+      <Triangle sideLength={sideL} rotation={0} left={sideL} top={0} color='gray' />
+
+      <Triangle sideLength={sideL} rotation={135} left={1.0625*sideL} top={0.355*sideL} color='red' />
+      <Triangle sideLength={sideL} rotation={225} left={2.385*sideL} top={1.0625*sideL} color='blue' />
+      <Triangle sideLength={sideL} rotation={45} left={0.355*sideL} top={0.6775*sideL} color='green' />
+      <Triangle sideLength={sideL} rotation={-45} left={1.6775*sideL} top={1.385*sideL} color='yellow' />
     </div>
   )
 }
--- src/Views/Triangle.js
+++ src/Views/Triangle.js
@@ -1,19 +1,31 @@
 import React from 'react'
 
-export const Triangle = ({ sideLength, rotation, left, top, color }) => {
-  let borderLengthArg = sideLength / 2 + 'px';
-  let leftPx = left + 'px';
-  let topPx = top + 'px';
+export const Triangle = ({sideLength, rotation, left, top, color}) => {
+  // let borderLengthArg = sideLength / 2 + 'px';
+
+  // alert(left)
+  
+  let borderLengthF = (sideLength + 0.0)/2; //I'm not sure why its half
+  // let rotationRadians = (rotation + .0)/360*2*Math.PI;
+  // alert(rotationRadians)
+  // arrowPLeft, arrowPTop
+  // let left = arrowPLeft - (sideLengthF * Math.cos(rotationRadians)) //According to chatGPT
+  // let top = arrowPTop - (sideLengthF * Math.sin(rotationRadians))
+  // let left = arrowPLeft - (sideLengthF/2 * Math.cos(rotationRadians))
+  // let top = arrowPTop -   (sideLengthF/2 * Math.sin(rotationRadians))
   return (
     // var transformValue = 'rotate(' + rotation + ')';
     <div className='triangle' style={{
       position: 'absolute',
       width: 0,
       height: 0,
-      borderTop: borderLengthArg + ' solid ' + color,
-      borderLeft: borderLengthArg + ' solid transparent',
-      borderRight: borderLengthArg + ' solid transparent',
-      transform: 'rotate(' + rotation + ')', left: leftPx, top: topPx
+      borderBottom: borderLengthF + 'px solid ' + color,
+      borderLeft: borderLengthF + 'px solid transparent',
+      borderRight: borderLengthF + 'px solid transparent',
+      transformOrigin: 'top left',
+      transform: 'rotate(' + rotation + 'deg)',
+      left: left + 'px',
+      top: top + 'px'
     }}> </div>
     /* border-left: 20px solid transparent;
         border-right: 20px solid transparent;
