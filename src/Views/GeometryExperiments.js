aa555cd34bc3e07a712cde53f1636e6e12559481
Tihifas <tdbratberg@gmail.com>
2023-05-07 13:28:19 +0200

    Some of last commit was not saved

--- src/Views/GeometryExperiments.js
+++ src/Views/GeometryExperiments.js
@@ -9,23 +9,46 @@ export const GeometryExperiments = () => {
   let sideL = 300 + .0;
   let katete = sideL * 0.707;
 
-  let containerWidth = 2*katete + sideL;
-  let containerHeight = 2*katete;
+  let containerWidth = 2 * katete + sideL;
+  let containerHeight = 2 * katete;
   // const {windowHeight, windowWidth} = useWindowDimensions()
   return (
-    <div style={{padding: '30px'}}>
+    <div style={{ padding: '30px' }}>
+      <div className='triangle-imgs-container' >
+        <img src={winnieSupperImg} />
+        <img src={winnieSupperImg} />
+        <img src={winnieSupperImg} />
+        <img src={winnieSupperImg} />
+        <img src={winnieSupperImg} />
+        <img src={winnieSupperImg} />
+      </div>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <div className='hex-image-container'>
         <img src={winnieSupperImg} />
         <div className='hex-image-text'> The last honey</div>
       </div>
-      <div className='layout-container blue-theme' style={{ marginLeft: '2%', marginTop: '5%',
-                                                            backgroundColor: 'lightblue', marginBottom: '30%',
-                                                            width: containerWidth + 'px', height: containerHeight + 'px' }}>
-        <QuadrantTriangle quadrant={2} color={'red'} width={'100px'} height={'100px'}/>
-        <QuadrantTriangle quadrant={1} color={'green'} width={'100px'} height={'100px'}/>
+      <div className='layout-container blue-theme' style={{
+        marginLeft: '2%', marginTop: '5%',
+        backgroundColor: 'lightblue', marginBottom: '30%',
+        width: containerWidth + 'px', height: containerHeight + 'px'
+      }}>
+        <QuadrantTriangle quadrant={2} color={'red'} width={'100px'} height={'100px'} />
+        <QuadrantTriangle quadrant={1} color={'green'} width={'100px'} height={'100px'} />
         <br />
-        <QuadrantTriangle quadrant={3} color={'blue'} width={'100px'} height={'100px'}/>
-        <QuadrantTriangle quadrant={4} color={'purple'} width={'100px'} height={'100px'}/>
+        <QuadrantTriangle quadrant={3} color={'blue'} width={'100px'} height={'100px'} />
+        <QuadrantTriangle quadrant={4} color={'purple'} width={'100px'} height={'100px'} />
       </div>
     </div>
   )
