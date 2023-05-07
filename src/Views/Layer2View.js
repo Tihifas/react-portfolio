44ea84c4c5546ae4967c918443183b241417e06a
Tihifas <tdbratberg@gmail.com>
2023-05-07 13:27:56 +0200

    Made hexagon out of trangular imgs on exspriements

--- src/Views/Layer2View.js
+++ src/Views/Layer2View.js
@@ -12,17 +12,17 @@ const Layer2View = () => {
     <>
     <ContentViewLayout>
         <h2>Cool stuff</h2>
-        <div>
-            <p>Brie cut the cheese bavarian bergkase. Stilton gouda stinking bishop say cheese cream cheese cheese on toast camembert de normandie parmesan. Stinking bishop pecorino emmental cheese and biscuits st. agur blue cheese cheese slices pecorino danish fontina. Edam st. agur blue cheese edam fondue when the cheese comes out everybody's happy cow port-salut ricotta. Cauliflower cheese.</p>
-            <p>Ricotta cheesy feet croque monsieur. Parmesan cheese triangles airedale boursin goat fromage boursin cheesy grin. Airedale red leicester cauliflower cheese cheesy grin cheese and wine bavarian bergkase gouda cheese and wine. Bocconcini hard cheese rubber cheese jarlsberg airedale.</p>
-        </div>
-        <div className='LinkBox-container'>
+        
+
+        
+
+        {/* <div className='LinkBox-container'>
             <LinkBox link="/koala" heading={"Koala C. Martin"} img={koalaImg} />
             <LinkBox link="/link1" heading={"The Supermarked"} img={guineaPigImg} />
             <LinkBox link="/link1" heading={"Look Away"} img={uglyDucklingImg} />
             <LinkBox link="/link1" heading={"A Crime Against Nature"} img={slenderCruellaImg} />
             <LinkBox link="/link1" heading={"Who framed Winnie?"} img={winnieSupperImg} />
-        </div>
+        </div> */}
     </ContentViewLayout>
     </>
   )
