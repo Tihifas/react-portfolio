022006b49f0f32da65da9816d8e69cb25616a1d0
Torstein Bratberg <tdbratberg@gmail.com>
2023-05-14 12:30:07 +0200

    Revert "Added eslint.json .tsx extension enforced, not .js"
    This reverts commit 42a1fa6ed55109376d58f8ba0a1152f1c87234d8.

--- src/Views/UglyDucklingView.tsx
+++ src/Views/UglyDucklingView.tsx
@@ -1,22 +1,13 @@
-import TsTest from '../Components/TsTest'
 import { ContentViewLayout } from './ContentViewLayout'
 
-type UglyDucklingParams = {
-  textParam: string
-}
-
-export const UglyDucklingView = ({textParam} : UglyDucklingParams ) => {
+export const UglyDucklingView = () => {
     return (
       <>
       <ContentViewLayout>
           <h2>The Ugly Duckling</h2>
           <div>
-            <h2>TsTest:</h2>
-            <TsTest textParam="textParamFromUgly" />
-            {/* <TsTest textParam={textParam} /> */}
-              {/* <p>{textParam}</p> */}
-              {/* <p>Once upon a time, in a parallel universe far, far away, there was a rather peculiar duckling named Quackers. Quackers had the most unusual appearance anyone had ever seen. His feathers were a mishmash of vibrant colors, ranging from neon pink to electric blue, with a few splotches of polka dots thrown in for good measure. While other ducks quacked gracefully, Quackers sounded more like a kazoo stuck in a blender. He was undoubtedly the ugly duckling of the pond.</p> */}
-              {/* <p>Quackers longed for acceptance and friendship. Every time he tried to waddle over to join the other ducks, they would scuttle away, giggling and whispering amongst themselves. Even the frogs would hop off their lily pads, fearing Quackers' strange appearance.</p> */}
+              <p>Once upon a time, in a parallel universe far, far away, there was a rather peculiar duckling named Quackers. Quackers had the most unusual appearance anyone had ever seen. His feathers were a mishmash of vibrant colors, ranging from neon pink to electric blue, with a few splotches of polka dots thrown in for good measure. While other ducks quacked gracefully, Quackers sounded more like a kazoo stuck in a blender. He was undoubtedly the ugly duckling of the pond.</p>
+              <p>Quackers longed for acceptance and friendship. Every time he tried to waddle over to join the other ducks, they would scuttle away, giggling and whispering amongst themselves. Even the frogs would hop off their lily pads, fearing Quackers' strange appearance. It was a lonely existence, and Quackers began to believe he was destined to be an outcast forever.</p>
           </div>
       </ContentViewLayout>
       </>
