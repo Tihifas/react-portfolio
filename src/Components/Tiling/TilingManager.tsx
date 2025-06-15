9c9630a3fa654c61d5a7d0f9a9ec75c5e7636f57
Tihifas <tdbratberg@gmail.com>
2025-06-15 20:26:10 +0200

    Made simple tiling of dots

--- src/Components/Tiling/TilingManager.tsx
+++ src/Components/Tiling/TilingManager.tsx
@@ -7,14 +7,28 @@ type TilingManagerProps = {
 }
 
 const TilingManager = ({ }: TilingManagerProps) => {
+    //Expanding vectors
+    const r = 20;
+    const r1 = new Vector(1.731 * r, -r);
+    const r2 = new Vector(0, -2 * r);
 
     const pos1 = new Vector(0, 0);
     const pos2 = new Vector(75, 75);
-    
+
+    const tilingElements = [];
+    for (let i = -5; i < 5; i++) {
+        for (let j = -5; j < 5; j++) {
+            const pos = Vector.add(r1.scale(i), r2.scale(j));
+            tilingElements.push(<TilingElement pos={pos} />);
+        }
+    }
+
     return (
-        <div style={{ position: 'relative' }}>
-            <TilingElement pos={pos1} />
-            <TilingElement pos={pos2} />
+        <div style={{ position: 'relative', width: '100%', height: '100%' }}>
+            <h2>Tiling Manager</h2>
+            <div style={{ position: 'absolute', left: '50%', top: '50%' }}>
+                {tilingElements}
+            </div>
         </div>
     )
 }
--- src/Views/TilingView.tsx
+++ src/Views/TilingView.tsx
@@ -3,7 +3,7 @@ import TilingManager from '../Components/Tiling/TilingManager.tsx';
 const TilingView = () => {
     return (
         <>
-        <div className='tiling-view' style={{ position: 'relative', width: '100%', height: '1600px', margin: '50px', border: '1px solid black' }}>
+        <div className='tiling-view' style={{ position: 'relative', width: '1080px', height: '720px', margin: '50px', border: '1px solid black' }}>
             <h1>Tiling View</h1>
             <TilingManager />
         </div>
